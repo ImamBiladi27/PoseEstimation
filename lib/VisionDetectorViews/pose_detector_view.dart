de5b42472c611b0dbf421808dac76e4ce7342a51
imam <imambiladi27@gmail.com>
2022-06-22 09:46:36 +0700

    PoseEstimationWithMLKitFlutter

---
+++ lib/VisionDetectorViews/pose_detector_view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+
+import 'camera_view.dart';
+import 'painters/pose_painter.dart';
+
+class PoseDetectorView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _PoseDetectorViewState();
+}
+
+class _PoseDetectorViewState extends State<PoseDetectorView> {
+  PoseDetector poseDetector = GoogleMlKit.vision.poseDetector();
+  bool isBusy = false;
+  CustomPaint? customPaint;
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await poseDetector.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CameraView(
+      title: 'Pose Detector',
+      customPaint: customPaint,
+      onImage: (inputImage) {
+        processImage(inputImage);
+      },
+    );
+  }
+
+  Future<void> processImage(InputImage inputImage) async {
+    if (isBusy) return;
+    isBusy = true;
+    final poses = await poseDetector.processImage(inputImage);
+    print('Found ${poses.length} poses');
+    for (Pose pose in poses){
+      pose.landmarks.forEach((_, landmark) {
+        final type = landmark.type;
+        final x = landmark.x;
+        final y = landmark.y;
+
+        print('IMAM type ${type}');
+
+        print('IMAM X ${x}');
+
+        print('IMAM Y ${y}');
+      });
+
+      final landmark = pose.landmarks[PoseLandmarkType.nose];
+    };
+    if (inputImage.inputImageData?.size != null &&
+        inputImage.inputImageData?.imageRotation != null) {
+      final painter = PosePainter(poses, inputImage.inputImageData!.size,
+          inputImage.inputImageData!.imageRotation);
+      customPaint = CustomPaint(painter: painter);
+    } else {
+      customPaint = null;
+    }
+    isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+}
